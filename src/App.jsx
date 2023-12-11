3133dfd91fca4a59db9f98151cba89fc93eef52c
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-12-11 07:40:23 +0600

    project-initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='h-20 text-3xl font-bold text-white bg-green-700'>
+      <h2>making portfolio</h2>
+    </div>
+  );
+};
+
+export default App;
